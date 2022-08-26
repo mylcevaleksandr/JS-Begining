fd6bffdb0d53e450119941eb8a47df5499f4a315
mylcevaleksandr <myltsevaleksandr@gmail.com>
2022-08-26 17:05:48 +0300

    Guess a word game

--- myScript.js
+++ myScript.js
@@ -16,6 +16,52 @@ for(i = 0; i < input.length; i++) {
 
     
 } 
-console.log(result);
+
 document.getElementById("jsAwesome").innerHTML = result.join("");
+
+let baseWords = [
+    "олень",
+    "лев",
+    "зебра",
+    "слон"
+];
+let currentWord = baseWords[Math.floor(Math.random() * baseWords.length)];
+let finalArray=[];
+let remainingLetters = currentWord.length;
+let tryCount = 15;
+
+
+for (let i = 0; i < currentWord.length; i++) {
+    finalArray[i] = "_";
+} 
+// alert (finalArray.join(" "));
+while (remainingLetters > 0 && tryCount > 0) {
+  
+    let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры. Подсказка: это животное");
+    
+    if (guess === null){
+        break;
+    } else if (guess.length !== 1) {
+        alert("Пожалуйста, введите только одну букву!");
+    } else {  
+        for (let j = 0; j < currentWord.length; j++) {
+            if(currentWord[j] === guess) {
+                finalArray[j] = guess;
+                remainingLetters--;
+                alert (finalArray.join(" ")); 
+               
+            } 
+           
+           
+        }
+    }
+    tryCount--
+    alert("Попыток осталось : " + tryCount + ".")
+    if(tryCount === 0){
+        alert("Вы проиграли.");
+    } else if (remainingLetters === 0) {
+        alert("Отлично! Было загадано слово ' " + currentWord + "'.")
+    }
+}
+
 
